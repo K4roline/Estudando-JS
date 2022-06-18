67091f299b7888c4de99ff09f3e326fd902d5b2a
Karoline Correa <ckaroline226@gmail.com>
2022-06-18 19:24:50 -0300

    Adicionando um novo projeto

---
+++ faixa-de-preco.js
@@ -0,0 +1,7 @@
+let faixas = [
+    {tooltip:'até R$700', minimo: 0, maximo: 700},
+    {tooltip:'de R$700 a 1000', minimo: 700, maximo: 1000}, 
+    {tooltip:'1000 ou mais', minimo: 1000, maximo: 99999}
+]
+
+console.log('faixas')
